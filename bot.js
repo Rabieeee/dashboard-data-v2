43bc0038db933c957d5655f79098e49bcb5bbe9b
Rabie <57900151+Rabieeee@users.noreply.github.com>
2020-08-31 18:56:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
-client.user.setActivity(`🍥 Rabieeeeeeee`)
 console.log(`Logged in as ${client.user.tag}!`);
 console.log("Streamstatus by DayZoonHD")
 
+client.user.setActivity(`🍥 Rabieeeeeeeer `, {
+type: "PLAYING",
+url: "https://www.twitch.tv/loltyler1"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
 });
+
 client.login(process.env.BOT_TOKEN);
