606b2283762320e3c2f21dd03f378cf381cacc3e
Rabie <57900151+Rabieeee@users.noreply.github.com>
2020-08-31 18:32:20 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+client.user.setActivity(`🍥`)
+console.log(`Logged in as ${client.user.tag}!`);
+console.log("Streamstatus by DayZoonHD")
+
+
+client.login(process.env.BOT_TOKEN);
